9caaa246f1ae175575af93af1bae7d465f10e8da
erkinov26 <erkinov0247@gmail.com>
2026-03-08 19:02:47 +0500

    clear

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { ThemeProvider } from "./context/theme-context";
-//commit
+import { ThemeProvider } from "./context/theme-context"; 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
