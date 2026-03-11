3d39d8817d0f37add5d10ecb860704281e498695
erkinov26 <erkinov0247@gmail.com>
2026-03-11 22:26:09 +0500

    commit

--- src/server/axios.ts
+++ src/server/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie"
 
-const BASE_URL = "/api/v1"; // shu yerni o'zgartir
+const BASE_URL = "/api/v1"; 
 
 const api = axios.create({
     baseURL: BASE_URL,
